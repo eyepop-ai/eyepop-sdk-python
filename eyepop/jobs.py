aa1ea554db4d3ca61ce83b9873c6e16421655942
Torsten Schulz <torsten.schulz@gmail.com>
2023-12-22 12:37:35 -0800

    Upon endpoint.disconnect() log collected metrics as DEBUG in category 'eyepop.metrics'.

--- eyepop/jobs.py
+++ eyepop/jobs.py
@@ -9,7 +9,7 @@ from typing import Callable
 import aiohttp
 from aiohttp import ClientSession
 
-log = logging.getLogger('eyepop')
+log_requests = logging.getLogger('eyepop.requests')
 
 
 class JobType(Enum):
@@ -17,19 +17,49 @@ class JobType(Enum):
     FROM_URL = 2
 
 
+class _JobStateCallback:
+    def created(self, job):
+        pass
+
+    def started(self, job):
+        pass
+
+    def first_result(self, job):
+        pass
+
+    def failed(self, job):
+        pass
+
+    def finished(self, job):
+        pass
+
+    def drained(self, job):
+        pass
+
+    def finalized(self, job):
+        pass
+
 class Job:
     """
     Abstract Job submitted to an EyePop.ai Endpoint.
     """
 
     def __init__(self, job_type: JobType, location: str, session: ClientSession,
-                 on_ready: Callable[["Job"], None] | None):
+                 on_ready: Callable[["Job"], None] | None, callback: _JobStateCallback | None = None):
         self.on_ready = on_ready
         self.job_type = job_type
         self.location = location
         self._session = session
         self._response = None
         self._queue = asyncio.Queue(maxsize=128)
+        if callback is not None:
+            self._callback = callback
+        else:
+            self._callback = _JobStateCallback()
+        self._callback.created(self)
+
+    def __del__(self):
+        self._callback.finalized(self)
 
     async def predict(self) -> dict:
         queue = self._queue
@@ -39,8 +69,10 @@ class Job:
             prediction = await queue.get()
             if prediction is None:
                 self._queue = None
+                self._callback.drained(self)
             elif isinstance(prediction, Exception):
                 self._queue = None
+                self._callback.drained(self)
                 raise prediction
             return prediction
 
@@ -57,6 +89,8 @@ class Job:
         queue = self._queue
         session = self._session
 
+        self._callback.started(self)
+
         try:
             await self._do_execute_job(queue, session)
             await queue.put(None)
@@ -65,23 +99,35 @@ class Job:
                 # we got canceled
                 pass
             else:
+                self._callback.failed(self)
                 await queue.put(e)
         finally:
             if self._response is not None:
                 response = self._response.close()
                 if response is not None:
                     response.release()
+            self._callback.finished(self)
             if self.on_ready:
                 await self.on_ready(self)
 
     async def _do_execute_job(self, queue: Queue, session: ClientSession):
         raise NotImplementedError("can't execute abstract jobs")
 
+    async def _do_read_response(self, queue: Queue):
+        got_results = False
+        async with self._response as response:
+            self._callback.first_result(self)
+            while line := await response.content.readline():
+                if not got_results:
+                    got_results = True
+                prediction = json.loads(line)
+                await queue.put(prediction)
+
 
 class _UploadJob(Job):
     def __init__(self, location: str, pipeline_base_url: str, authorization_header: str, session: ClientSession,
-                 on_ready: Callable[[Job], None] | None = None):
-        super().__init__(JobType.UPLOAD, location, session, on_ready)
+                 on_ready: Callable[[Job], None] | None = None, callback: _JobStateCallback | None = None):
+        super().__init__(JobType.UPLOAD, location, session, on_ready, callback)
         mime_types = mimetypes.guess_type(location)
         if len(mime_types) > 0:
             mime_type = mime_types[0]
@@ -97,17 +143,17 @@ class _UploadJob(Job):
 
     async def _do_execute_job(self, queue: Queue, session: ClientSession):
         with open(self.location, 'rb') as file:
+            log_requests.debug("before POST %s with file %s as body", self._target_url, self.location)
             self._response = await session.post(self._target_url, headers=self._headers, data=file,
                                                 timeout=self.timeouts)
-            while line := await self._response.content.readline():
-                prediction = json.loads(line)
-                await queue.put(prediction)
+            await self._do_read_response(queue)
+            log_requests.debug("after POST %s with file %s as body", self._target_url, self.location)
 
 
 class _LoadFromJob(Job):
     def __init__(self, location: str, pipeline_base_url: str, authorization_header: str, session: ClientSession,
-                 on_ready: Callable[[Job], None] | None = None):
-        super().__init__(JobType.FROM_URL, location, session, on_ready)
+                 on_ready: Callable[[Job], None] | None = None, callback: _JobStateCallback | None = None):
+        super().__init__(JobType.FROM_URL, location, session, on_ready, callback)
         self._target_url = f'{pipeline_base_url}/source?mode=queue&processing=sync'
         self._headers = {
             'Accept': 'application/jsonl',
@@ -120,8 +166,8 @@ class _LoadFromJob(Job):
         self.timeouts = aiohttp.ClientTimeout(total=None, sock_read=60)
 
     async def _do_execute_job(self, queue: Queue, session: ClientSession):
+        log_requests.debug("before PATCH %s with url %s as source", self._target_url, self.location)
         self._response = await session.patch(self._target_url, headers=self._headers, json=self._body,
                                              timeout=self.timeouts)
-        while line := await self._response.content.readline():
-            prediction = json.loads(line)
-            await queue.put(prediction)
+        await self._do_read_response(queue)
+        log_requests.debug("after PATCH %s with url %s as source", self._target_url, self.location)
