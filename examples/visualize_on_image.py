f49b0bb1cb232b628e68f085df38ece17ca4efc2
Torsten Schulz <torsten.schulz@gmail.com>
2024-01-15 06:23:27 -0800

    [chore] adding base unit-test and rename package eyepop-sdk-python -> eyepop

--- examples/visualize_on_image.py
+++ examples/visualize_on_image.py
@@ -4,8 +4,7 @@ from PIL import Image
 
 import matplotlib.pyplot as plt
 
-from eyepop.eyepopsdk import EyePopSdk
-from eyepop.visualize import EyePopPlot
+from eyepop import EyePopSdk
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger('eyepop.requests').setLevel(level=logging.DEBUG)
@@ -14,11 +13,10 @@ example_image_path = sys.argv[1]
 
 with EyePopSdk.endpoint() as endpoint:
     result = endpoint.upload(example_image_path).predict()
-    with Image.open(example_image_path) as image:
-        plt.imshow(image)
-        plot = EyePopPlot(plt.gca())
-        for obj in result['objects']:
-            plot.object(obj)
-    plt.show()
+with Image.open(example_image_path) as image:
+    plt.imshow(image)
+plot = EyePopSdk.plot(plt.gca())
+plot.prediction(result)
+plt.show()
 
 
