106f45b8d1e9a194beb9e570af3324eb157c955c
Zhanchong Deng <zhanchong@eyepop.ai>
2024-03-21 00:07:04 -0700

    wip

--- eyepop/endpoint.py
+++ eyepop/endpoint.py
@@ -135,15 +135,14 @@ class Endpoint:
         log_requests.debug('after GET %s', get_url)
         log_requests.debug('current popComp is %s', self.popComp)
 
-    async def pop_comp(self):
+    async def get_pop_comp(self) -> str:
         if self.popComp is None:
             await self.connect()
         return self.popComp
     
-    async def change_pop_comp(self, popComp: str = None):
+    async def set_pop_comp(self, popComp: str = None):
         if self.worker_config is None:
             await self.connect()
-        log_requests.debug('change_pop_comp(%s)', popComp)
         headers = {
             'Authorization': await self.__authorization_header(),
             'Content-Type': 'application/json'
@@ -157,9 +156,12 @@ class Endpoint:
             response.raise_for_status()
         log_requests.debug('after PATCH %s', patch_url)
         self.popComp = popComp
-        return response
     
-    async def list_models(self):
+    '''
+    Start Block
+    Below methods are not meant for the end user to use directly. They are used by the SDK internally.
+    '''
+    async def list_models(self) -> dict:
         if self.worker_config is None:
             await self.connect()
         headers = {
@@ -172,8 +174,63 @@ class Endpoint:
         async with self.session.get(get_url, headers=headers) as response:
             response.raise_for_status()
             return await response.json()
-
     
+    async def get_manifest(self) -> dict: 
+        if self.worker_config is None:
+            await self.connect()
+        headers = {
+            'Authorization': await self.__authorization_header(),
+        }
+        base_url = urljoin(self.eyepop_url, self.worker_config['base_url']).rstrip("/")
+        get_url = f'{base_url}/models/sources'
+        log_requests.debug('before GET %s', get_url)
+        async with self.session.get(get_url, headers=headers) as response:
+            response.raise_for_status()
+            return await response.json()
+    
+    async def set_manifest(self, manifest: dict):
+        if self.worker_config is None:
+            await self.connect()
+        headers = {
+            'Authorization': await self.__authorization_header(),
+            'Content-Type': 'application/json'
+        }
+        base_url = urljoin(self.eyepop_url, self.worker_config['base_url']).rstrip("/")
+        put_url = f'{base_url}/models/sources'
+        log_requests.debug('before PUT %s', put_url)
+        async with self.session.put(put_url, headers=headers, json=manifest) as response:
+            response.raise_for_status()
+        log_requests.debug('after PUT %s', put_url)
+
+    async def load_model(self, model:dict, override:bool = False):
+        if override:
+            await self.purge_model(model)
+        if self.worker_config is None:
+            await self.connect()
+        headers = {
+            'Authorization': await self.__authorization_header(),
+            'Content-Type': 'application/json'
+        }
+        base_url = urljoin(self.eyepop_url, self.worker_config['base_url']).rstrip("/")
+        post_url = f'{base_url}/models/instances'
+        log_requests.debug('before POST %s', post_url)
+        async with self.session.post(post_url, headers=headers, json=model) as response:
+            response.raise_for_status()
+        log_requests.debug('after POST %s', post_url)
+
+    async def purge_model(self, model:dict):
+        headers = {
+            'Authorization': await self.__authorization_header(),
+            'Content-Type': 'application/json'
+        }
+        base_url = urljoin(self.eyepop_url, self.worker_config['base_url']).rstrip("/")
+        delete_url = f'{base_url}/models/instances'
+        log_requests.debug('before DELETE %s', delete_url)
+        async with self.session.delete(delete_url, headers=headers, json=model) as response:
+            response.raise_for_status()
+    '''
+    End Block
+    '''
 
     def __pipeline_base_url(self):
         base_url = urljoin(self.eyepop_url, self.worker_config['base_url']).rstrip("/")
