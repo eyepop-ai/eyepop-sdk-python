98d5b268b8d7614d0aef839447621d500121151a
Torsten Schulz <torsten.schulz@gmail.com>
2024-05-07 18:50:50 -0700

    [feature] client side load balancing for pops in production mode

--- eyepop/exceptions.py
+++ eyepop/exceptions.py
@@ -1,7 +1,23 @@
 class PopNotStartedException(Exception):
     """
-    Thrown when the given Pop was not started and the Endpoint was requestred with auto_start=False
+    Thrown when the given Pop was not started and the Endpoint was requested with auto_start=False
     """
     def __init__(self, pop_id: str):
         super().__init__(f'Pop {pop_id} has not been started')
+
+
+class PopConfigurationException(Exception):
+    """
+    Thrown when the given Pop configuration was inconsistent 
+    """
+    def __init__(self, pop_id: str, reason: str):
+        super().__init__(f'Pop {pop_id} configuration issue: {reason}')
+
+
+class PopNotReachableException(Exception):
+    """
+    Thrown when no healthy endpoint is available
+    """
+    def __init__(self, pop_id: str, endpoints: list[dict]):
+        super().__init__(f'No healthy endpoint for Pop {pop_id} tried: {endpoints}')
 
