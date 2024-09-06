5d1698add542454283439b333e18e41fc6edfb86
Torsten Schulz <torsten.schulz@gmail.com>
2024-09-06 09:27:51 -0700

    [fix] NPE for request_tracer_max_buffer=0

---
+++ tests/data/test_endpoint_no_tracing.py
@@ -0,0 +1,34 @@
+import json
+import unittest
+from aiohttp import ClientResponseError
+from aioresponses import aioresponses, CallbackResult
+
+from eyepop import EyePopSdk
+from tests.data.base_endpoint_test import BaseEndpointTest
+
+
+class TestEndpointConnect(BaseEndpointTest):
+
+    @aioresponses()
+    def test_no_tracing(self, mock: aioresponses):
+
+        self.setup_base_mock(mock)
+
+        mock.post(f'{self.test_eyepop_url}/authentication/token', status=200, body=json.dumps(
+            {'expires_in': 1000 * 1000, 'token_type': 'Bearer', 'access_token': self.test_access_token}))
+        endpoint = EyePopSdk.dataEndpoint(
+            eyepop_url=self.test_eyepop_url,
+            secret_key=self.test_eyepop_secret_key,
+            account_id=self.test_eyepop_account_id,
+            request_tracer_max_buffer=0
+        )
+        try:
+            endpoint.connect()
+        finally:
+            endpoint.disconnect()
+
+        self.assertBaseMock(mock)
+
+
+if __name__ == '__main__':
+    unittest.main()
