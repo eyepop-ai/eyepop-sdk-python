cfad1b2c8fed45c2cb89cad635df9b9c87772980
Torsten Schulz <torsten@eyepop.ai>
2025-06-23 15:22:57 -0700

    [example] more options for caption example

--- examples/caption_demo.py
+++ examples/caption_demo.py
@@ -1,23 +1,11 @@
 import argparse
-import ast
-import base64
 import json
 import logging
 import os
 import sys
-from io import BytesIO
-from typing import BinaryIO
-
-from webui import webui
-from pybars import Compiler
-
-import requests
-from PIL import Image
 
 from eyepop import EyePopSdk
-from eyepop.data.data_types import TranscodeMode
-from eyepop.worker.worker_types import Pop, InferenceComponent, PopForward, PopForwardOperator, ForwardOperatorType, \
-    PopCrop, ContourFinderComponent, ContourType, CropForward, FullForward, ComponentParams
+from eyepop.worker.worker_types import Pop, InferenceComponent, FullForward
 
 script_dir = os.path.dirname(__file__)
 
@@ -46,6 +34,10 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-l', '--local-path', required=False, type=str, default=False, help="run the inference on a local file")
 parser.add_argument('-a', '--asset-uuid', required=False, type=str, default=False, help="run the inference on an asset by its Uuid")
 parser.add_argument('-u', '--url', required=False, type=str, default=False, help="run the inference on a remote Url")
+parser.add_argument('-c', '--caption-ability', type=str, default="eyepop.vlm.preview:latest", help="run this ability to generate captions", choices=["eyepop.vlm.preview:latest", "eyepop.image-caption.preview.qwen2:latest"])
+parser.add_argument('-r', '--role', type=str, default=None, required=False, help="use this role to generate the caption")
+parser.add_argument('-p', '--prompt', type=str, default=None, required=False, help="use this prompt to generate the caption")
+parser.add_argument('-q', '--question', type=str, default=None, required=False, help="apply this question to the caption")
 
 
 args = parser.parse_args()
@@ -55,28 +47,41 @@ if not args.local_path and not args.url and not args.asset_uuid:
     parser.print_help()
     sys.exit(1)
 
+forward = None
+if args.question is not None:
+    forward = FullForward(
+        targets=[InferenceComponent(
+            categoryName='answers',
+            model='eyepop.question-answer.preview:latest',
+            params={
+                "prompt": args.question
+            }
+        )]
+    )
+
+caption_params = dict()
+if args.role is not None:
+    caption_params['role'] = args.role
+if args.prompt is not None:
+    caption_params['prompt'] = args.prompt
+
+caption_pop = Pop(components=[
+    InferenceComponent(
+        categoryName='captions',
+        ability=args.caption_ability,
+        forward=forward,
+        params=caption_params,
+    )
+])
+
 with EyePopSdk.workerEndpoint() as endpoint:
     endpoint.set_pop(caption_pop)
     if args.local_path:
         job = endpoint.upload(args.local_path)
-        while result := job.predict():
-           visualize_result = result
-           if args.output:
-                logging.getLogger('eyepop.example').info(json.dumps(result, indent=2))
-        if args.visualize:
-            image = Image.open(args.local_path)
-            buffer = BytesIO()
-            image.save(buffer, format="PNG")
-            example_image_src = f"data:image/png;base64, {base64.b64encode(buffer.getvalue()).decode()}"
     elif args.url:
         job = endpoint.load_from(args.url)
-        while result := job.predict():
-            logging.getLogger('eyepop.example').info(json.dumps(result, indent=2))
-            with requests.get(args.url) as response:
-                image = Image.open(BytesIO(response.content))
-            example_image_src = args.url
     elif args.asset_uuid:
         job = endpoint.load_asset(args.asset_uuid)
-        while result := job.predict():
-            logging.getLogger('eyepop.example').info(json.dumps(result, indent=2))
+    while result := job.predict():
+        logging.getLogger('eyepop.example').info(json.dumps(result, indent=2))
 
