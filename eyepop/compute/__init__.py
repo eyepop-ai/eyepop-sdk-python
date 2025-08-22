1b87c31e947ec6080ac48ff39ccd41dafdb1bc3e
Ryan <github@pocketcereal.com>
2025-08-22 08:27:41 -0700

    Revert "Merge pull request #88 from eyepop-ai/feat/compute-api-url"
    This reverts commit b5ccc05079ad5f0e0a49655deab36feff089fbb7, reversing
    changes made to c56b25f0f7fda2cfed342b19438a65a82c004361.

--- eyepop/compute/__init__.py
+++
@@ -1,4 +0,0 @@
-from .api import fetch_session_endpoint
-from .models import ComputeApiSessionResponse, ComputeContext
-
-__all__ = ["fetch_session_endpoint", "ComputeApiSessionResponse", "ComputeContext"]
