7c17d601e592df08edf1880e7cbce2fb48f38939
archae0pteryx <github@pocketcereal.com>
2026-01-21 12:30:38 -0800

    Use eyepop.person:latest ability for integration test

--- tests/integration/test_worker_endpoint.py
+++ tests/integration/test_worker_endpoint.py
@@ -15,7 +15,7 @@ from eyepop.worker.worker_types import InferenceComponent, Pop
 
 PUBLIC_TEST_IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/800px-Cat_November_2010-1a.jpg"
 TEST_POP = Pop(components=[
-    InferenceComponent(ability="eyepop-person:EPPersonB1_Person_TorchScriptCpu_float32")
+    InferenceComponent(ability="eyepop.person:latest")
 ])
 
 
